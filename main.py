a892529498cd4b926564a3be6bb6ba4927d043b3
J0l-lN_TiT0R <J0l-lN_TiT0R@pm.me>
2021-11-16 12:07:04 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# Get the page
+source = requests.get('https://books.toscrape.com').text
+soup = BeautifulSoup(source, 'html.parser')
+# print(soup.prettify())
+
+# Finding useful information
+for item in soup.find_all('article', class_='product_pod'):
+    title = item.h3.a.text
+    price = item.find('p', class_='price_color').text[1:]
+    rating = item.p['class'][-1]
+    print(title, price, f"Rating: {rating}", sep='\n', end='\n\n')
+
